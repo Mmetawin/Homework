252b7862a0d61f41180c15870c898e23149bf39b
Mmetawin <3159141037@qq.com>
2021-11-21 15:28:21 +0800

    [feature]Wmsir类写了个加法

--- src/Wmsir.java
+++ src/Wmsir.java
@@ -1,5 +1,7 @@
-public class jaskah {
+public class Wmsir {
     public static void main(String[] args){
         System.out.println("你成功了");
+        int a=3,b=2;
+        System.out.println("a+b="+(a+b));
     }
 }
